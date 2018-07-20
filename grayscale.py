ba245a1aa119939a1bfe6b089f6fe8d863f6164f
AlessandroMessori <allemex@libero.it>
2018-07-20 23:08:56 +0200

    added numpy and drawing tests

--- grayscale.py
+++ grayscale.py
@@ -1,9 +1,8 @@
-import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -11,8 +10,8 @@ while(True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
-    cv2.imshow('gray',gray)
+    cv2.imshow('frame', frame)
+    cv2.imshow('gray', gray)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
