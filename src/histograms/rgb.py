3dde117a8ac73c82f796a49c6782c851de5797f6
AlessandroMessori <allemex@libero.it>
2018-08-21 00:56:19 +0200

    added exercise on mask histogram

--- src/histograms/rgb.py
+++ src/histograms/rgb.py
@@ -7,7 +7,7 @@ img = cv2.imread(path)
 
 color = ('b', 'g', 'r')
 for i, col in enumerate(color):
-    histr = cv2.calcHist([img], [i], None, [256], [0, 256])
-    plt.plot(histr, color=col)
+    hist = cv2.calcHist([img], [i], None, [256], [0, 256])
+    plt.plot(hist, color=col)
     plt.xlim([0, 256])
 plt.show()
