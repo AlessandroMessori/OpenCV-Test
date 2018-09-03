822121ed34805a1225522eb033d54c46677ff2f5
AlessandroMessori <allemex@libero.it>
2018-09-04 01:26:05 +0200

    added exercises on Brute Force Matcher,Flann Matcher and Homography

---
+++ src/featureDetection/BFMatcher.py
@@ -0,0 +1,23 @@
+import cv2
+from matplotlib import pyplot as plt
+from ..utils.media import get_image_path
+
+pathname1 = get_image_path('ronaldo.jpeg')
+pathname2 = get_image_path('match.jpeg')
+img1 = cv2.imread(pathname1, 0)  # queryImage
+img2 = cv2.imread(pathname2, 0)  # trainImage
+
+# Initiate SIFT detector
+orb = cv2.ORB_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = orb.detectAndCompute(img1, None)
+kp2, des2 = orb.detectAndCompute(img2, None)
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)  # Match descriptors.
+matches = bf.match(des1, des2)
+# Sort them in the order of their distance.
+matches = sorted(matches, key=lambda x: x.distance)
+
+img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], outImg=img1)
+plt.imshow(img3), plt.show()
